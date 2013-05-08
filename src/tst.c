d7d58a8aea34ae8e602d87c18676c8f91570d244
Matt Baker <mdbaker@uwaterloo.ca>
2013-05-08 01:48:11 -0400

    Minor changes to tst.c and added tst.h

--- src/tst.c
+++ src/tst.c
@@ -1,7 +1,14 @@
-#include <bwio.h>
-#include <ts7200.h>
+#include <bwio.h>	//io library
+#include <ts7200.h>	//Predefined constants
 
-int main(){
+int main(int argc, char * argv[]){
+	//PURPOSE:
+	//	Compile
+	//	Communicate properly to terminal (COM_2)
+	//	Output something to terminal
+	char** outString = "Hello\n\r";
+	bwsetfifo(2, OFF);
+	
 	bwputstr(2, "Test");
 
 	return 0;
